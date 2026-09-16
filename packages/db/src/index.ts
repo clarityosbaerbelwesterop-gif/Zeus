44a44acf99796709791c982b7af5501b6649f842
clarityosbaerbelwesterop-gif <clarityos.baerbelwesterop@gmail.com>
2026-09-16 09:14:21 +0200

    feat(db): export TeamRun schema

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -2,3 +2,4 @@ export { closeDatabase, withActor, type ActorDatabase } from "./client";
 export * from "./m4-schema";
 export * from "./runtime-schema";
 export * from "./schema";
+export * from "./team-schema";
