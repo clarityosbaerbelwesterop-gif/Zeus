544fa0e5b84f0a89a9766c32421a93382f548e25
clarityosbaerbelwesterop-gif <clarityos.baerbelwesterop@gmail.com>
2026-09-16 11:04:20 +0200

    feat(db): export V1 operating models

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,5 +1,6 @@
 export { closeDatabase, withActor, type ActorDatabase } from "./client";
 export * from "./m4-schema";
+export * from "./operating-schema";
 export * from "./runtime-schema";
 export * from "./schema";
 export * from "./team-schema";
